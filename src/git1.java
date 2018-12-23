965ab2228bff946c930d46998835b75a87cebd81
= <=>
2018-12-23 11:41:10 +0200

    fourth commit

--- src/git1.java
+++ src/git1.java
@@ -3,7 +3,7 @@ public class git1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("heello");
-        System.out.println("welcome");
+        System.out.println("welco");
        
     }
 }
